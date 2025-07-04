ff4662d081758633328a570ca3530f02669699f7
camiladelrey <louyze.rodrigues@escola.pr.gov.br>
2025-07-04 11:17:15 +0000

    aula 02

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEvebotaoMostraPalavrasntListener('click' ,mostrapalavraschaves);
+ 
+function mostrapalavraschaves(){
+    alert('Deu boa')
+}
